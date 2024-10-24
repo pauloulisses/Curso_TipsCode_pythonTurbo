10da40569c5bdb2646d94088bee2d0175acc23e4
paulo <pauloulisses1212@gmail.com>
2024-10-24 19:48:15 -0300

    operador lógico

---
+++ MOD-03/operador_logico.py
@@ -0,0 +1,38 @@
+"""
+Operador AND (E lógico)
+
+O operador AND retorna True apenas se 
+ambos os operandos forem True.
+
+ A	       B	      A and B
+True	True	       True
+True	False	       False
+False	True	       False
+False	False	       False
+
+"""
+
+"""
+Operador OR (OU lógico)
+O operador OR retorna True se pelo menos 
+um dos operandos for True.
+
+A	     B	       A or B
+True	True	   True
+True	False	   True
+False	True	   True
+False	False	   False
+
+
+"""
+
+"""
+Operador NOT (Negação lógica)
+O operador not inverte o valor 
+lógico de uma expressão.
+
+A	   not     A
+True	     False
+False	     True
+
+"""
